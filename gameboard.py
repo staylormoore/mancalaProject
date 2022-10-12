31197b8a6c94eb56402255f0c52b1c7e3d0aaf8b
Sam <samtaylormoore@datamachines.io>
2022-10-12 09:18:23 -0400

    Update to Gameboard class

--- gameboard.py
+++ gameboard.py
@@ -1,3 +1,7 @@
 class Gameboard:
-    def __init__(self , g ):
+    def __init__(self, g):
         self.gameboard = g
+
+    p1pits = [4, 4, 4, 4, 4, 4, 0]  # initializes p1's side with 4 seeds in each pit, last element = their mancala
+    p2pits = [0, 4, 4, 4, 4, 4, 4]  # initializes p2's side with 4 seeds in each pit, first element = their mancala
+
