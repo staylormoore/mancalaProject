cd9807d71f714ed976784ac9d658f413854691f4
Sam <samtaylormoore@datamachines.io>
2022-10-14 08:48:39 -0400

    Update changes

--- game.py
+++ game.py
@@ -4,5 +4,8 @@ from gameboard import Gameboard
 
 
 class Game:
-    g1 = Gameboard()
+    g1 = Gameboard()  # mancala gameboard
+    p1 = Human()  # player1
+    p2 = Human()  # player2
+
     print(g1)
