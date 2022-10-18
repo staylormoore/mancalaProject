d15dead0ee78f9aa99c9cbfd51119401158a9934
Sam <samtaylormoore@datamachines.io>
2022-10-18 08:34:41 -0400

    game class

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ from gameboard import Gameboard
 
 
 class Game:
-    g1 = Gameboard()  # mancala board
+    self.g1 = Gameboard()  # mancala board
     p1 = Human("Player1")  # player1
     p2 = Human("Player2")  # player2
     turn = True  # when turn = True, it is p1's turn, when turn = False, it is p2's turn
@@ -14,9 +14,10 @@ class Game:
 
 
 def determine_winner():
-    p1seeds = Gameboard.p1pits[0]  # FIX THIS - use accessor methods?
-    p2seeds = Gameboard.p2pits[7]
+    p1seeds = self.g1.p1pits[0]  # FIX THIS - use accessor methods?
+    p2seeds = g1.p2pits[7]
     if p1seeds > p2seeds:
         print("Player 1 wins!")
     else:
         print("Player 2 wins!")
+
