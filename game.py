1c5cd4df1e9af553970fdb57a82d4925d399af2a
mburrell2 <mburrell@potomacschool.org>
2022-10-18 08:53:54 -0400

    Game Class update

--- game.py
+++ game.py
@@ -10,15 +10,13 @@ class Game:
         self.p2 = Human("Player2")  # player2
         self.turn = True  # when turn = True, it is p1's turn, when turn = False, it is p2's turn
 
-    print(self.g1.__str__())
-    print(self.p1.__str__())
+        print(self.g1.__str__())
+        print(self.p1.__str__())
 
-
-def determine_winner():
-    p1seeds = self.g1.get_p1pits()  # FIX THIS - use accessor methods?
-    p2seeds = self.g1.get_p2pits()
-    if p1seeds > p2seeds:
-        print("Player 1 wins!")
-    else:
-        print("Player 2 wins!")
-
+    def determine_winner(self):
+        p1seeds = self.g1.get_p1pits()  # FIX THIS - use accessor methods?
+        p2seeds = self.g1.get_p2pits()
+        if p1seeds[0] > p2seeds[6]:
+            print("Player 1 wins!")
+        else:
+            print("Player 2 wins!")
